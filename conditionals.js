d492bc0533cc62a64b68226449c1d36635cac911
praveenbham <muvgipra@gmail.com>
2022-11-30 09:25:47 +0530

    Conditional_JS

---
+++ conditionals.js
@@ -0,0 +1,22 @@
+// if statement -satisfy condition//
+
+let text = 'hello';
+if (text === "hello") {
+   text = text + ' world';
+}
+console.log(text === 'hello world');
+
+let num = 0;
+if (1 < 2) {
+   num = 1;
+}
+console.log(num === 1);
+
+//  if...else statement - run else  //
+
+if (2 < 1) {
+   num = 1;
+} else {
+   num = 2;
+}
+console.log(num === 2);
